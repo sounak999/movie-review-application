2e421e00ffa6eca2a240615bc64d8b696289e56a
Sounak <sounaksaha333@gmail.com>
2025-06-01 23:58:02 +0530

    added support for GET single movie by Imdb Id

--- backend/src/main/java/dev/sounak/movies/controller/MovieController.java
+++ backend/src/main/java/dev/sounak/movies/controller/MovieController.java
@@ -2,14 +2,17 @@ package dev.sounak.movies.controller;
 
 import dev.sounak.movies.entity.Movie;
 import dev.sounak.movies.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -22,5 +25,10 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies() {
         List<Movie> allMovies = movieService.getAllMovies();
         return new ResponseEntity<>(allMovies, HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId) {
+        return new ResponseEntity<>(movieService.getMovieByImdbId(imdbId), HttpStatus.OK);
     }
 }
--- backend/src/main/java/dev/sounak/movies/service/MovieService.java
+++ backend/src/main/java/dev/sounak/movies/service/MovieService.java
@@ -2,10 +2,12 @@ package dev.sounak.movies.service;
 
 import dev.sounak.movies.entity.Movie;
 import dev.sounak.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,5 +17,9 @@ public class MovieService {
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
